0aafceb5a7d05edb922ffb3bd3d369dd907ca3ba
Xianguang-Zhou <xianguang.zhou@outlook.com>
2021-01-04 12:53:15 +0800

    Add the command to run a Librian project.

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import * as path from 'path';
 import * as vscode from 'vscode';
 import * as highlight from './highlight';
 
@@ -18,11 +19,23 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('liber-vscode.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('liber-vscode.runLibrianProject', () => {
 		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from liber-vscode!');
+		const docUri = vscode.window.activeTextEditor?.document.uri;
+		let workspaceFolder: vscode.WorkspaceFolder | undefined = undefined;
+		if (docUri !== undefined) {
+			workspaceFolder = vscode.workspace.getWorkspaceFolder(docUri);
+		}
+		if (workspaceFolder === undefined) {
+			if (vscode.workspace.workspaceFolders !== undefined) {
+				workspaceFolder = vscode.workspace.workspaceFolders[0];
+			}
+		}
+		const projectPath = workspaceFolder !== undefined ? workspaceFolder.uri.fsPath : path.resolve('.');
+		console.debug(`Run Librian project "${projectPath}".`);
+		const terminal = vscode.window.createTerminal('Run Librian Project', 'python',
+			['-m', 'librian', '--project', projectPath]);
+		terminal.show(true);
 	});
 
 	context.subscriptions.push(disposable);
